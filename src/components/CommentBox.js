48e4b7819a145a75d10942b9d3d0d7274dc3420b
Dmytro <dmytro.osiychuk@gmail.com>
2022-03-09 22:30:44 +0100

    Add render tests with Enzyme

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default () => {
-  return <div> commentBox</div>;
+  return <div>Comment Box</div>;
 };
--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { shallow } from 'enzyme';
 import App from '../../App';
+import CommentBox from '../CommentBox';
 
 it('shows a comment box', () => {
   //JSDOM emulates a DOM element in the terminal
-  const div = document.create('div');
+  const div = document.createElement('div');
   //check whether App does not crash at render
   ReactDOM.render(<App />, div);
   //Check any other functionality of the App component
+  //literal testing whether the component contains some specific HTML
+  //bad approach because it delves into the contents of the inner component, test fails if anything changes in child component
+  expect(div.innerHTML).toContain('Comment Box');
 
   //cleanup after the test has run
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('contains a comment box', () => {
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
 });
