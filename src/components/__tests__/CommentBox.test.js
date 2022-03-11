814c50770fe3fca073de22b12873704697ece7a8
Dmytro <dmytro.osiychuk@gmail.com>
2022-03-11 19:12:54 +0100

    Write tests to simulate events and retrieve props

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,4 +1,26 @@
 import React from 'react';
+import { mount } from 'enzyme';
 import CommentBox from 'components/CommentBox';
 
-it('has a textarea and a button', () => {});
+let wrapped;
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
+
+it('has a textarea and a button', () => {
+  expect(wrapped.find('textarea').length).toEqual(1);
+  expect(wrapped.find('button').length).toEqual(1);
+});
+afterEach(() => {
+  //we need to unmount after each test because the JSDOM is shared across all tests
+  wrapped.unmount();
+});
+
+it('has a textarea that the user can type in synced with react', () => {
+  wrapped.find('textarea').simulate('change', {
+    target: { value: 'new comment' }, //simlating the event.target.value change
+  });
+  //force component re-render in order to avoid async re-render (setState)
+  wrapped.update();
+  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+});
